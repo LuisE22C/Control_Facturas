3d4635fa2937cb3ce526a0b537ee9052c70c19ac
LCirilo <lciriloa@miumg.edu.gt>
2025-10-27 02:13:00 -0600

    Cambios

--- Backend/ControlFacturasAPI/ControlFacturasAPI/obj/Debug/net8.0/ControlFacturasAPI.AssemblyInfo.cs
+++ Backend/ControlFacturasAPI/ControlFacturasAPI/obj/Debug/net8.0/ControlFacturasAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlFacturasAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aae6927dab337c41d0dce063b19f5043fab29c95")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlFacturasAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControlFacturasAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
